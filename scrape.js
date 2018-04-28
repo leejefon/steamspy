9227bea08a357059831a413a1c50b4afe7c90530
Jeff Lee <jlee@silver-peak.com>
2018-04-28 14:25:29 -0700

    Scrape all pages

--- scrape.js
+++ scrape.js
@@ -4,13 +4,21 @@ const cheerio = require('cheerio');
 const steamSearchUrl = 'https://store.steampowered.com/search/?vrsupport=101%2C102%2C104';
 const totalPage = 123;
 
-requestify
-  .get(steamSearchUrl)
-  .then((response) => {
-    const $ = cheerio.load(response.getBody());
+const requests = Array.from({ length: totalPage }, (v, x) => x + 1)
+  .map(page => requestify.get(`${steamSearchUrl}&page=${page}`));
 
+Promise.all(requests).then(results => {
+  const data = results.map((html) => {
+    const $ = cheerio.load(html.getBody(), { decodeEntities: false });
+    const apps = []
     $('.search_result_row').each((i, elem) => {
-      console.log($(elem).data('dsAppid'));
-      console.log($(elem).find('.ellipsis .title').html())
+      apps.push({
+        id: $(elem).data('dsAppid'),
+        name: $(elem).find('.ellipsis .title').html()
+      });
     });
-  });
+    return apps;
+  }).reduce((prev, curr) => prev.concat(curr), []);
+
+  console.log(data);
+});
