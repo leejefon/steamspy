6f1c1fb9c10428ffad10faf183a1bd59e77cfa71
Jeff Lee <jlee@silver-peak.com>
2018-05-13 11:44:40 -0700

    Added top tags in table

--- client/src/components/AppsTable.jsx
+++ client/src/components/AppsTable.jsx
@@ -3,6 +3,7 @@ import { BootstrapTable as Table, TableHeaderColumn as Header } from 'react-boot
 import { connect } from 'react-redux';
 import 'react-bootstrap-table/css/react-bootstrap-table.css';
 import TagCloud from '../components/TagCloud';
+import * as Utils from '../utils';
 
 class AppsTable extends Component {
   constructor() {
@@ -17,14 +18,25 @@ class AppsTable extends Component {
     this.setState({ filterByTag });
   }
 
+  topThreeTags(game) {
+    const iterator = Utils.getTopTag(game.tags);
+    const first = iterator.next().value;
+    const second = iterator.next().value;
+    const third = iterator.next().value;
+
+    return [first, second, third];
+  }
+
   render() {
     const options = {
       paginationSize: 10,
       sizePerPage: 50
     };
 
-    const data = this.props.games.filter(game => !this.state.filterByTag.value ||
-      Object.keys(game.tags).includes(this.state.filterByTag.value));
+    const data = this.props.games
+      .filter(game => !this.state.filterByTag.value ||
+        Object.keys(game.tags).includes(this.state.filterByTag.value))
+      .map(game => Object.assign({}, game, { topTags: this.topThreeTags(game).join(', ') }));
 
     const title = this.state.filterByTag.value ? (
       <h1><b>{this.state.filterByTag.value}</b> Games</h1>
@@ -40,6 +52,7 @@ class AppsTable extends Component {
           <Header width="80" dataField="appid" isKey>App ID</Header>
           <Header width="160" dataField="developer" dataSort>Developer</Header>
           <Header width="160" dataField="publisher" dataSort>Publisher</Header>
+          <Header width="250" dataField="topTags" dataSort>Top Tags</Header>
           <Header width="110" dataField="score_rank" dataSort>Score Rank</Header>
           <Header width="100" dataField="positive" dataSort>Positive</Header>
           <Header width="100" dataField="negative" dataSort>Negative</Header>
