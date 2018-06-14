7f5dce40f342b5344eeed3a6fbd638237a3871c7
Jeff Lee <jlee@silver-peak.com>
2018-06-13 17:16:15 -0700

    Added player stats data to the api

--- index.js
+++ index.js
@@ -5,14 +5,32 @@ const Datastore = require('nedb');
 const app = express(feathers());
 app.configure(express.rest());
 
-const db = new Datastore({ filename: './data/appDetails.db' });
-db.loadDatabase();
+const appsDb = new Datastore({ filename: './data/appDetails.db' });
+appsDb.loadDatabase();
+
+const playerStatsDb = new Datastore({ filename: './data/playerCounts.db' });
+playerStatsDb.loadDatabase();
 
 class Games {
 	async find() {
-		return new Promise(resolve => {
-			db.find().exec((err, docs) => {
+		const appsP = new Promise((resolve) => {
+			appsDb.find().exec((err, docs) => {
 				resolve(docs);
+			});
+		});
+
+		const playerStatsP = new Promise((resolve) => {
+			playerStatsDb.find().exec((err, docs) => {
+				resolve(docs);
+			});
+		});
+
+		return Promise.all([appsP, playerStatsP]).then((results) => {
+			const [apps, playerStats] = results;
+
+			return apps.map((app) => {
+				const { stats } = playerStats.find((stat) => stat.id == app.appid) || {}; // Double equale since stat.id is string
+				return Object.assign({ stats }, app);
 			});
 		});
 	}
